47e9aa3b7e5aac960f0d04250ecfc59171a6abae
ronywahyuu <ronywahyuu@upi.edu>
2022-12-03 19:25:22 +0700

    navbar

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar";
+import Nav from "../Nav";
+// import Navbar from "../Navbar";
 import SocialMedia from "../SocialMedia";
 
 const MainLayout = () => {
   return (
-    <div className=" text-white ">
-      <Navbar />
-      <div className="text-white  bg-gradient-to-b from-primary to-secondary min-h-screen">
+    <div className=" text-white">
+      <Nav />
+      <div className="text-white  bg-gradient-to-b from-primary to-secondary min-h-screen font-montserrat">
         <SocialMedia />
         <Outlet />
       </div>
